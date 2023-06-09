63bf4e5eb1e436de34b40f2bf4c7d5b516176841
mubashiralam89 <mubashiralam89@gmail.com>
2023-06-09 20:27:08 +0500

    added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <>
       <Calculator />
+      <footer className="mb-10 mt-6">
+        <p className="text-center font-semibold text-neutral-900 text-xl">
+          Made By "Mubashir Alam"
+        </p>
+      </footer>
     </>
   );
 }
